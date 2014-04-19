7f906be4b9dc00d054ef02208b366914401108c7
SiMing Weng <siming.weng@gmail.com>
2014-04-19 14:09:29 +0800

    do not retry if there happens an exception when contacting the server

--- indoortmsclient/src/main/java/com/bnj/indoortms/api/client/request/BaseRequest.java
+++ indoortmsclient/src/main/java/com/bnj/indoortms/api/client/request/BaseRequest.java
@@ -4,6 +4,7 @@
 package com.bnj.indoortms.api.client.request;
 
 import com.octo.android.robospice.request.googlehttpclient.GoogleHttpClientSpiceRequest;
+import com.octo.android.robospice.retry.DefaultRetryPolicy;
 
 /**
  * @param <T>
@@ -11,10 +12,11 @@ import com.octo.android.robospice.request.googlehttpclient.GoogleHttpClientSpice
  */
 public abstract class BaseRequest<T> extends GoogleHttpClientSpiceRequest<T> {
 
-    protected String host = "http://10.66.96.103:3000";
+    protected String host = "http://192.168.1.182:3000";
 
     public BaseRequest(Class<T> clazz) {
         super(clazz);
+        setRetryPolicy(new DefaultRetryPolicy(0, 0, 0));
     }
 
     public abstract String getCacheKey();
