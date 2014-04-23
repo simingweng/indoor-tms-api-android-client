46a28c539a83f2064ecf3821c0d0625d7d304f94
SiMing Weng <siming.weng@gmail.com>
2014-04-23 22:28:59 +0800

    raw floor plan image is stored under the building folder instead of under each floor folder on the tile server

--- indoortmsclient/src/main/java/com/bnj/indoortms/api/client/utils/FloorImageUtil.java
+++ indoortmsclient/src/main/java/com/bnj/indoortms/api/client/utils/FloorImageUtil.java
@@ -4,7 +4,7 @@ package com.bnj.indoortms.api.client.utils;
  * Created by wen55527 on 4/22/14.
  */
 public class FloorImageUtil {
-    private static final String imageUrlTemplate = "http://192.168.1.182/%s/%s/%s";
+    private static final String imageUrlTemplate = "http://192.168.1.182/%s/%s";
     private static FloorImageUtil ourInstance = new FloorImageUtil();
 
     private FloorImageUtil() {
@@ -14,7 +14,7 @@ public class FloorImageUtil {
         return ourInstance;
     }
 
-    public String getImageUrl(String buildingId, String floorId, String image) {
-        return String.format(imageUrlTemplate, buildingId, floorId, image);
+    public String getImageUrl(String buildingId, String image) {
+        return String.format(imageUrlTemplate, buildingId, image);
     }
 }
