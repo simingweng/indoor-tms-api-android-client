c3b6988eb2b56a44cebc64a3e6924ff8a737ae47
Simon Weng <siming.weng@gmail.com>
2014-04-25 16:37:39 +0800

    use a single multipart request to create a new floor

--- indoortmsclient/src/main/java/com/bnj/indoortms/api/client/request/CreateFloorRequest.java
+++ indoortmsclient/src/main/java/com/bnj/indoortms/api/client/request/CreateFloorRequest.java
@@ -1,10 +1,15 @@
 package com.bnj.indoortms.api.client.request;
 
 import com.bnj.indoortms.api.client.model.Floor;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.MultipartContent;
+import com.google.api.client.http.MultipartFormDataContent;
 import com.google.api.client.http.json.JsonHttpContent;
 import com.google.api.client.json.gson.GsonFactory;
+
+import java.io.File;
 
 /**
  * Created by wen55527 on 4/23/14.
@@ -14,11 +19,13 @@ public class CreateFloorRequest extends BaseRequest<Floor> {
     private static final String url = "/api/buildings/%s/floors";
     private Floor floor;
     private String buildingId;
+    private File newFloorPlan;
 
-    public CreateFloorRequest(String buildingId, Floor floor) {
+    public CreateFloorRequest(String buildingId, Floor floor, String floorPlan) {
         super(Floor.class);
         this.buildingId = buildingId;
         this.floor = floor;
+        this.newFloorPlan = new File(floorPlan);
     }
 
     @Override
@@ -28,13 +35,16 @@ public class CreateFloorRequest extends BaseRequest<Floor> {
 
     @Override
     public Floor loadDataFromNetwork() throws Exception {
-        HttpRequest request = getHttpRequestFactory().buildPostRequest(
-                new GenericUrl(host + String.format(url, buildingId)),
-                new JsonHttpContent(GsonFactory.getDefaultInstance(),
-                        floor)
-        );
-        request.setParser(GsonFactory.getDefaultInstance()
-                .createJsonObjectParser());
+        MultipartFormDataContent multipart = new MultipartFormDataContent();
+        MultipartContent.Part filePart = new MultipartContent.Part(new FileContent("image/jpeg",
+                newFloorPlan));
+        multipart.addPart(filePart, "image", newFloorPlan.getName());
+        MultipartContent.Part jsonPart = new MultipartContent.Part(new JsonHttpContent
+                (GsonFactory.getDefaultInstance(), floor));
+        multipart.addPart(jsonPart, "floor", null);
+        HttpRequest request = getHttpRequestFactory().buildPostRequest(new GenericUrl(host +
+                String.format(url, buildingId)), multipart);
+        request.setParser(GsonFactory.getDefaultInstance().createJsonObjectParser());
         return request.execute().parseAs(getResultType());
     }
 }
